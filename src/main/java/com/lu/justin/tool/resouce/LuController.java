78e08f6c726d49ba68128fe5d697d75519dbb427
Justin <lijt2007@hotmail.com>
2018-08-19 17:25:00 +0800

    update rate

--- src/main/java/com/lu/justin/tool/resouce/LuController.java
+++ src/main/java/com/lu/justin/tool/resouce/LuController.java
@@ -89,7 +89,7 @@ public class LuController {
                     e.printStackTrace();
                 }
                 LocalDateTime _e = LocalDateTime.now();
-                log.info("Get total count:{} ms[from:{} to:{}]", Duration.between(_s, _e), _s, _e);
+                log.info("Get success rate:{} ms[from:{} to:{}]", Duration.between(_s, _e), _s, _e);
             }));
         }
 
