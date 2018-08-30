1c73ac13ceb5cb3ba3ff8db991c508c130464e43
Justin <lijt2007@hotmail.com>
2018-08-30 23:40:24 +0800

    fix bug

--- src/main/java/com/lu/justin/tool/dao/ProductSummaryDAO.java
+++ src/main/java/com/lu/justin/tool/dao/ProductSummaryDAO.java
@@ -4,8 +4,11 @@ import com.lu.justin.tool.dao.dto.ProductSummaryDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface ProductSummaryDAO extends MongoRepository<ProductSummaryDTO, Long> {
 
+    ProductSummaryDTO findByDate(Date date);
 
 }
