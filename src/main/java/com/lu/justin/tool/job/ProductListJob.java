fc626b9c6b8d26207bf979f343474433da46b3af
Jiantao Li <lijt2007@hotmail.com>
2018-09-08 12:06:41 +0800

    Update ProductListJob.java
    add imprt

--- src/main/java/com/lu/justin/tool/job/ProductListJob.java
+++ src/main/java/com/lu/justin/tool/job/ProductListJob.java
@@ -14,6 +14,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.scheduling.annotation.Scheduled;
